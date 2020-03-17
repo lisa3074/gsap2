f7555573b077e5f1e657d225d60c4771972d1c52
Lisa Søndergaard <lisa3074@stud.kea.dk>
2020-03-17 14:30:48 +0100

    all three exercises

--- beachball.js
+++ beachball.js
@@ -1,7 +1,14 @@
 "use strict";
 /* import { gsap } from "gsap"; */
 import { gsap } from "gsap/dist/gsap";
-const h1 = document.querySelector("h1");
-const ball = document.querySelector(".ball");
 
-gsap.to(h1.current, { color: "#fff", duration: 2 });
+/* gsap.to(".ball", { x: 400, duration: 2 });
+gsap.to(".ball", { rotate: 200, duration: 2 });
+gsap.to(".ball", { delay: 2, scale: 1.4, duration: 1 });
+gsap.to(".ball", { delay: 3, scale: 0, duration: 1 });
+gsap.to(".ball", { delay: 4, scale: 1, rotate: -200, x: 0, duration: 2 }); */
+
+const timeline = gsap.timeline({ repeat: 2, repeatDelay: 0 });
+
+timeline.to(".ball", { x: 400, duration: 2, rotate: 200, duration: 2 });
+timeline.to(".ball", { scale: 0, x: 1000, y: -2000, duration: 1 });
--- multiballs.js
+++ multiballs.js
@@ -1,1 +1,5 @@
 "use strict";
+
+import { gsap } from "gsap";
+
+gsap.to(".ball", { y: -400, stagger: 0.2, duration: 2, repeat: 3, yoyo: true });
