e6e9909dadf3f3444d435dfb4219a91600096c55
Lisa Søndergaard <lisa3074@stud.kea.dk>
2020-03-17 13:29:04 +0100

    gsap problems

--- beachball.js
+++ beachball.js
@@ -1,1 +1,7 @@
 "use strict";
+/* import { gsap } from "gsap"; */
+import { gsap } from "gsap/dist/gsap";
+const h1 = document.querySelector("h1");
+const ball = document.querySelector(".ball");
+
+gsap.to(h1.current, { color: "#fff", duration: 2 });
